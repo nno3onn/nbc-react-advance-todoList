31bdce186fb6af07d583613b9c8b970e112be64d
nno3onn <nno3onn@naver.com>
2022-12-21 12:58:42 +0900

    feat: set up redux-thunk

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+export const __getTodos = createAsyncThunk("todos/getTodos", async (payload, thunkAPI) => {
+  try {
+    const data = await axios.get("http://localhost:3001/todos");
+    return thunkAPI.fulfillWithValue(data.data);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [__getTodos.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__getTodos.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.todos = action.payload;
+    },
+    [__getTodos.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {} = todosSlice.actions;
+export default todosSlice.reducer;
